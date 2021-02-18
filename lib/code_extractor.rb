26e65e81b04ea44428166b30865dfa2a073e3c46
Nick LaMuro <nicklamuro@gmail.com>
2021-02-18 14:00:31 -0600

    [CodeExtractor] Use pycall + git-filter-repo
    Switches over to using `git-filter-repo` through pycall, but maintains
    the same interface for working with `code-extractor`

--- lib/code_extractor.rb
+++ lib/code_extractor.rb
@@ -1,4 +1,5 @@
 require 'yaml'
+require 'fileutils'
 
 # Class to extract files and folders from a git repository, while maintaining
 # The git history.
@@ -12,7 +13,6 @@ module CodeExtractor
     def initialize(config_file = 'extractions.yml')
       @config = YAML.load_file(config_file)
 
-      @config[:destination]       = File.expand_path(@config[:destination])
       @config[:upstream_branch] ||= "master"
 
       validate!
@@ -28,7 +28,7 @@ module CodeExtractor
     alias to_s inspect
 
     def validate!
-      missing = %i[name destination upstream upstream_name extractions].reject { |k| @config[k] }
+      missing = %i[name upstream upstream_name extractions].reject { |k| @config[k] }
       raise ArgumentError, "#{missing.map(&:inspect).join(", ")} key(s) missing" if missing.any?
     end
   end
@@ -36,9 +36,18 @@ module CodeExtractor
   class GitProject
     attr_reader :name, :url, :git_dir, :new_branch, :source_branch
 
-    def initialize name, url
+    def initialize name, url = nil
       @name   = name
       @url    = url
+    end
+
+    def init
+      @git_dir ||= name
+
+      FileUtils.rm_rf git_dir if Dir.exist?(git_dir)
+
+      puts 'Initializing git dir…'
+      system "git init -b master #{git_dir}"
     end
 
     def clone_to destination, origin_name = "upstream"
@@ -85,17 +94,39 @@ module CodeExtractor
       end
     end
 
-    def filter_branch extractions, upstream_name
-      Dir.chdir git_dir do
-        `time git filter-branch --index-filter '
-        git read-tree --empty
-        git reset $GIT_COMMIT -- #{extractions}
-        ' --msg-filter '
-        cat -
-        echo
-        echo
-        echo "(transferred from #{upstream_name}@$GIT_COMMIT)"
-        ' -- #{source_branch} -- #{extractions}`
+    def filter_repo extractions, source_project
+      code_extraction_args = %W[
+        --source          #{source_project.git_dir}
+        --target          #{git_dir}
+        --refs            #{source_project.source_branch}
+        --commit-callback #{commit_callback source_project.name}
+      ]
+
+      extractions.each { |path| code_extraction_args << "--path" << path }
+
+      repo_filter_args = git_filter_repo.FilteringOptions.parse_args(code_extraction_args)
+      filter           = git_filter_repo.RepoFilter.new(repo_filter_args)
+
+      puts "running filter..."
+      filter.run
+    rescue PyCall::PyError => py_err
+      exit if py_err.type.to_s == "<class ='SystemExit'>"
+      raise
+    end
+
+    def commit_callback upstream_name
+      text = "transferred from #{upstream_name}@"
+      'commit.message = commit.message + b"\n\n(' + text + '"+ commit.original_id + b")"'
+    end
+
+    def git_filter_repo
+      return @git_filter_repo if defined?(@git_filter_repo)
+
+      require 'pycall/import'
+
+      unless PyCall.sys.path[-1].include?("git-filter-repo")
+        PyCall.sys.path.append(File.join(__dir__, "git-filter-repo"))
+        @git_filter_repo = PyCall.import_module :git_filter_repo
       end
     end
   end
@@ -103,20 +134,34 @@ module CodeExtractor
   class Runner
     def initialize config = nil
       @config         = config || Config.new
-      @source_project = GitProject.new @config[:name], @config[:upstream]
-    end
-
-    def extractions
-      @extractions ||= @config[:extractions].join(' ')
+      @source_project = GitProject.new @config[:upstream_name], @config[:upstream]
+      @target_project = GitProject.new @config[:name], @config[:upstream]
     end
 
     def extract
       puts @config
-      @source_project.clone_to @config[:destination]
-      @source_project.extract_branch @config[:upstream_branch], "extract_#{@config[:name]}", extractions
-      @source_project.remove_remote
-      @source_project.remove_tags
-      @source_project.filter_branch extractions, @config[:upstream_name]
+      @target_project.init
+      @source_project.clone_to upstream_dirname
+      @source_project.extract_branch upstream_branch, "extract_#{@config[:name]}", extraction_list
+      @target_project.filter_repo extractions, @source_project
+    end
+
+    private
+
+    def extraction_list
+      @config[:extractions].join(' ')
+    end
+
+    def extractions
+      @config[:extractions]
+    end
+
+    def upstream_branch
+      @config[:upstream_branch]
+    end
+
+    def upstream_dirname
+      "#{@config[:upstream_name].split("/").last}_source"
     end
   end
 end
